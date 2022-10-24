a41fe241ab35fa17ff66959911d5a2c628ff615b
valkim55 <vesper5521@gmail.com>
2022-10-24 00:13:40 -0400

    server connection works

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const {User} = require('../../models');
 // ===== GET all users => /api/users =====
 router.get('/', (req, res) => {
     // access the User model and run .findAll() method = SELECT * FROM users
-    User.findAll()
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // ===== GET a single user => /api/users/id =====
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: {exclide: ['password']},
         where: {
             id: req.params.id // SELECT * FROM users WHERE id=?;
         }
