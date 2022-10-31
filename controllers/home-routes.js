cf9e598b2683a7f680176e0a7f1a9120b428865d
valkim55 <vesper5521@gmail.com>
2022-10-30 22:31:31 -0400

    homepage created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,42 @@
 //this file will contain all of the user-facing routes - homepage and login pages
 
+const sequelize = require('../config/connection');
+const {Post, Comment, User} = require('../models');
+
 //setup main homepage route
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: { model: User, attributes: ['username'] }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        // pass a single post object in to the homepage template
+        //console.log(dbPostData[0])
+        const posts = dbPostData.map(post => {
+            return post.get({ plain: true });
+        })
+        res.render('homepage', { posts });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
